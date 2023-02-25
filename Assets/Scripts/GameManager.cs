67dde6a9bd42b9ea6f9ce321c8c50c0b4dad9efa
michon <michon@sparkslab.me>
2023-02-25 15:08:58 +0400

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    int score;
+    public static GameManager instance;
+
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] Player playerMovement;
+
+    public void IncrementScore ()
+    {
+        score++;
+        scoreText.text = score.ToString();
+        // Increase the player's speed
+        playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    }
+
+    private void Awake ()
+    {
+        instance = this;
+    }
+
+    private void Start () {
+
+	}
+
+	private void Update () {
+	
+	}
+}
