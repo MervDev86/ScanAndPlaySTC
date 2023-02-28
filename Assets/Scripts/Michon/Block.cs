0010bf20deccac8179d2589c17edf12e9ec97012
michon <michon@sparkslab.me>
2023-02-28 22:56:33 +0400

    Cleaned up Block Script

--- Assets/Scripts/Michon/Block.cs
+++ Assets/Scripts/Michon/Block.cs
@@ -1,28 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    MeshRenderer m_renderer;
     BoxCollider m_collider;
+
+    [Header("Spawner")]
+    [SerializeField] GameObject m_coinPrefab;
+    [SerializeField] GameObject m_ObstaclePrefab;
+    [SerializeField] int obstacleSpawnLimit = 1;
     [Space]
-    [SerializeField] float offset;
-    [SerializeField] Vector3 origin;
-    [Space]
+
     [Header("Grid")]
-    [SerializeField] int row;
-    [SerializeField] int column;
-    Vector3[,] points;
+    //xPoints is supposed to be based off the movement points of the player
+    [SerializeField] float[] xPoints = new float[3];
+    [SerializeField] int row;//DEFAULT = 3  => FOR NOW KEEP THE SAME VAL
+    [SerializeField] int column = 3;
     [Space]
-    [Header("Debug")]
+    [SerializeField] float zOffset;
+    Vector3[,] spawnPoints;
+    [Space]
+
+    [Header("Item Spawn Visualizer")]
     [SerializeField] float d_sphereSize = 0.3f;
     [SerializeField] Color d_color = Color.white;
     [Space]
     [Header("Block Visualizer")]
     [SerializeField] bool showNextBlockPreview = true;
     [SerializeField] int previewLimit = 2;
-    [SerializeField] Vector3 previewOffset;
     [SerializeField] Color previewColor = Color.white;
 
     public float maxXBounds;
@@ -30,58 +37,55 @@ public class Block : MonoBehaviour
 
     private void OnValidate()
     {
-        if (m_renderer == null)
-            m_renderer = GetComponent<MeshRenderer>();
         if (m_collider == null)
             m_collider = GetComponent<BoxCollider>();
 
-        points = new Vector3[row, column];
-        var pointDiffX = m_collider.bounds.max.x / row;
-        var pointDiffY = m_collider.bounds.max.z / row;
+        InitGridLayout();
+    }
 
-        maxXBounds = m_renderer.bounds.max.x;
-        maxYBounds = m_renderer.bounds.max.z;
+    private void Start()
+    {
+        SpawnItems();
+    }
 
+    void InitGridLayout()
+    {
+        //GetXpoints();
+        spawnPoints = new Vector3[row, column];
+
+        //var pointDiffX = m_collider.bounds.max.x / row;
+        var pointDiffY = m_collider.bounds.max.z / column;
+
+        //maxXBounds = m_collider.bounds.max.x;
+        maxYBounds = m_collider.bounds.max.z;
         for (int pointXIndex = 0; pointXIndex < row; pointXIndex++)
         {
             for (int pointYIndex = 0; pointYIndex < column; pointYIndex++)
             {
-                var xVal = offset - pointXIndex * pointDiffX;
-                var yVal = pointYIndex * pointDiffY;
-                points[pointXIndex, pointYIndex] = new Vector3(xVal, yVal);
+                //var xVal = offset - pointXIndex * pointDiffX;
+                var yVal = (pointYIndex * pointDiffY) + zOffset;
+                spawnPoints[pointXIndex, pointYIndex] = new Vector3(xPoints[pointXIndex], transform.position.y, yVal);
             }
         }
-        Debug.Log($"total Values loaded: {points.Length}");
+        //Debug.Log($"total Values loaded: {points.Length}");
     }
 
-    [ContextMenu("Show Values")]
-    public void ShowValues()
+    void GetXpoints()
     {
-        string info = "";
-        for (int xIndex = 0; xIndex < row; xIndex++)
+        var playerMovementPointDelta = GameManager.instance.GetMovementDistance;
+        for (int xPointIndex = 0; xPointIndex < row; xPointIndex++)
         {
-            for (int yIndex = 0; yIndex < column; yIndex++)
-            {
-
-                info += $" {points[xIndex, yIndex]}";
-            }
-            info += "\n";
+            xPoints[xPointIndex] = xPointIndex * playerMovementPointDelta;
         }
-        Debug.Log(info);
     }
 
-    //Get Spawn Points
-    // Start is called before the first frame update
-    void Start()
+    void SpawnItems()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
+        foreach (var spawnPosition in spawnPoints)
+        {
+            var obj = Instantiate(m_coinPrefab, transform, false);
+            obj.transform.position = spawnPosition;
+        }
     }
 
     #region Debugger
@@ -95,9 +99,9 @@ public class Block : MonoBehaviour
     void ShowSpawnPointVisualizer()
     {
         Gizmos.color = d_color;
-        foreach (var item in points)
+        foreach (var item in spawnPoints)
         {
-            Gizmos.DrawSphere(new Vector3(item.x, 0, item.y), d_sphereSize);
+            Gizmos.DrawSphere(item, d_sphereSize);
         }
     }
 
@@ -110,9 +114,25 @@ public class Block : MonoBehaviour
             for (int i = 0; i < previewLimit; i++)
             {
                 //Gizmos.DrawCube(transform.position , m_collider.size);
-                Gizmos.DrawCube(transform.position + new Vector3(transform.position.x, transform.position.y, m_collider.size.z * i) , m_collider.size);
+                Gizmos.DrawCube(transform.position + new Vector3(transform.position.x, transform.position.y, m_collider.size.z * i), m_collider.size);
             }
         }
+    }
+
+    [ContextMenu("Show Values")]
+    public void ShowValues()
+    {
+        string info = "";
+        for (int xIndex = 0; xIndex < row; xIndex++)
+        {
+            for (int yIndex = 0; yIndex < column; yIndex++)
+            {
+
+                info += $" {spawnPoints[xIndex, yIndex]}";
+            }
+            info += "\n";
+        }
+        Debug.Log(info);
     }
     #endregion
 
