550380330dea80bb773a175ab3482ee8739fc40b
rainnedev <rainnedev@gmail.com>
2023-03-10 21:59:48 +0400

    Added multiplayer sync and end game

--- Assets/Scripts/NetworkClientHandler/SessionsHandler.cs
+++ Assets/Scripts/NetworkClientHandler/SessionsHandler.cs
@@ -10,63 +10,52 @@ namespace NetworkClientHandler
 {
     public class SessionsDataRef
     {
-        public static string IS_PLAYING = "/isPlaying";
+        public const string PLAYER_1 = "player1";
+        public const string PLAYER_2 = "player2";
+        public const string GAME_STATE = "gameState";
+        public const string PLAYER_COUNT = "playerCount";
     }
 
     public class SessionsHandler : MonoBehaviour
     {
 
-        //public static SessionsHandler instance;
-
-        public static Action<bool> OnStartPlaying;
-        
-        //MERVIN ADDED
-        public static Action<int> OnInitializeGame; //Set how many players
-
-        #region PLAYER_Movement
-        public static Action<int> OnMovePlayer1;
-        public static Action<string> OnPlayer1SetName;
-        
-        public static Action<int> OnMovePlayer2;
-        public static Action<string> OnPlayer2SetName;
-
-
-        #endregion PLAYER_Movement
-
-
-        private static string DB_SESSIONS = "stcrunner_sessions";
-        private static string DB_REGISTRATIONS = "stcrunner_registrations";
-
-        private static string REF_SESSION_ISPLAYING = "/isPlaying";
-        private static string REF_SESSION_ISMULTIPLAYER = "/isMultiplayer";
-
-        private static string REF_SESSION_PLAYER1 = "/player1";
-        private static string REF_SESSION_PLAYER2 = "/player2";
-
-        private FirebaseApp m_App;
-        private FirebaseDatabase m_DB;
-
-        private DatabaseReference m_sessionsDB;
-        private DatabaseReference m_registrationsDB;
-        private DatabaseReference m_refCurrentPosP1;
-        private DatabaseReference m_refUuidPosP1;
-        private DatabaseReference m_refNameP1;
-
-        private bool m_isPlaying = false;
-        private bool m_isMultiplayer = false;
-
-
-        //private DatabaseReference m_player1;
 
         DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
         protected bool isFirebaseInitialized = false;
 
+        //public static Action<bool> OnStartPlaying;
 
-        private static string m_namePlayer1 = "";
-        public static string GetPlayerName1()
-        {
-            return m_namePlayer1;
-        }
+        //MERVIN ADDED
+        public static Action<int> OnInitializeGame;     //Set how many players
+        public static Action OnStartGame;               // Set to GameState.PLAYING
+
+        #region PLAYER_Movement
+        public static Action<int> OnMovePlayer1;
+        public static Action<string> OnPlayer1SetName;
+
+        public static Action<int> OnMovePlayer2;
+        public static Action<string> OnPlayer2SetName;
+        #endregion PLAYER_Movement
+
+        private static string DB_SESSIONS = "stcrunner_sessionsdebug";
+        private static string DB_REGISTRATIONS = "stcrunner_registrationsdebug";
+
+        private FirebaseApp m_App;
+        private FirebaseDatabase m_DB;
+        private DatabaseReference m_sessionsDB;
+
+        //private DatabaseReference m_registrationsDB;
+
+        //private DatabaseReference m_refCurrentPosP1;
+        //private DatabaseReference m_refUuidPosP1;
+        //private DatabaseReference m_refNameP1;
+
+        //private bool m_isPlaying = false;
+        //private bool m_isMultiplayer = false;
+        //private DatabaseReference m_player1;
+
+        private int m_playerCount = 0;
+
 
         // Start is called before the first frame update
         void Start()
@@ -97,7 +86,7 @@ namespace NetworkClientHandler
             m_sessionsDB.ChildAdded += HandleSessionsChildAdded;
             m_sessionsDB.ChildChanged += HandleSessionsChildChanged;
 
-            m_registrationsDB = m_DB.GetReference(DB_REGISTRATIONS);
+            //m_registrationsDB = m_DB.GetReference(DB_REGISTRATIONS);
 
             isFirebaseInitialized = true;
         }
@@ -112,32 +101,21 @@ namespace NetworkClientHandler
             }
             if (args.Snapshot != null)
             {
+                Debug.Log("dbsessions.onChildAdded = " + args.Snapshot.Key);
                 switch (args.Snapshot.Key)
                 {
-                    case "player1":
-                        //Debug.Log("[PLAYER_1 :: added] uuid = " + args.Snapshot.Child("uuid").Value);
-                        string uuidP1 = (string) args.Snapshot.Child("uuid").Value;
-                        m_registrationsDB.Child(uuidP1).GetValueAsync().ContinueWithOnMainThread(task =>
-                        {
-                            if (task.IsCompleted)
-                            {
-                                string player1Name = (string)task.Result.Child("firstname").Value;
-                                OnPlayer1SetName?.Invoke(player1Name); //Initialize Player when name is added
-                            }
-                        });
+                    case SessionsDataRef.PLAYER_1:
+                        //Debug.Log("[PLAYER_1 :: added] uuid = " + args.Snapshot.Child("name").Value);
+
+                        string player1Name = args.Snapshot.Child("name").Value.ToString();
+                        OnPlayer1SetName?.Invoke(player1Name); //Initialize Player when name is added
                         break;
 
-                    case "player2":
-                        Debug.Log("[PLAYER_2 :: added] uuid = " + args.Snapshot.Child("uuid").Value);
-                        string uuidP2 = (string) args.Snapshot.Child("uuid").Value;
-                        m_registrationsDB.Child(uuidP2).GetValueAsync().ContinueWithOnMainThread(task =>
-                        {
-                            if (task.IsCompleted)
-                            {
-                                string player2Name = (string)task.Result.Child("firstname").Value;
-                                OnPlayer2SetName?.Invoke(player2Name); //Initialize Player when name is added
-                            }
-                        });
+                    case SessionsDataRef.PLAYER_2:
+                        //Debug.Log("[PLAYER_2 :: added] uuid = " + args.Snapshot.Child("name").Value);
+
+                        string player2Name = args.Snapshot.Child("name").Value.ToString();
+                        OnPlayer2SetName?.Invoke(player2Name); //Initialize Player when name is added
                         break;
                 }
             }
@@ -151,51 +129,71 @@ namespace NetworkClientHandler
             }
             if (args.Snapshot != null)
             {
-                GameState state = GameManager.instance.GetCurrentGameState();
-                if (args.Snapshot.Key == "isPlaying")
+                Debug.Log("dbsessions.onChildChanged = " + args.Snapshot.Key + " | " + args.Snapshot.Value);
+                switch (args.Snapshot.Key)
                 {
-                    //Debug.Log("[ChildChanged] isPlaying = " + args.Snapshot.Value);
-                    m_isPlaying = (bool) args.Snapshot.Value;
-                    if (m_isPlaying) OnStartPlaying?.Invoke(false);
-                }
-                if (args.Snapshot.Key == "player1")
-                {
-                    if (!m_isPlaying && state.Equals(GameState.GAME_OVER))
-                    {
-                        m_sessionsDB.ChildChanged -= HandleSessionsChildChanged;
-                        OnMovePlayer1?.Invoke(1);
-                    } 
-                    else
-                    {
-                        if (args.Snapshot.Child("currentPosition").Key == "currentPosition")
+                    case SessionsDataRef.PLAYER_COUNT:
                         {
-                            //Debug.Log("[ChildChanged] player1.currentPosition = " + args.Snapshot.Child("currentPosition").Value);
-                            var position = int.Parse(args.Snapshot.Child("currentPosition").Value.ToString());
-                            if (position == -1) OnMovePlayer1?.Invoke(0);
-                            else if (position == 1) OnMovePlayer1?.Invoke(2);
-                            else OnMovePlayer1?.Invoke(1);  // (position == 0)
+                            m_playerCount = int.Parse(args.Snapshot.Value.ToString());
+                            if (m_playerCount > 0)
+                            {
+                                OnInitializeGame?.Invoke(m_playerCount);
+                            }
+                            break;
                         }
-                    }
+
+                    case SessionsDataRef.GAME_STATE:
+                        {
+                            string gameState = (string)args.Snapshot.Value;
+                            switch (gameState)
+                            {
+                                case "WAITING":
+                                    break;
+
+                                case "PLAYING":
+                                    OnStartGame?.Invoke();
+                                    break;
+
+                                case "END":
+                                    break;
+
+                                case "IDLE":
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+
+                    case SessionsDataRef.PLAYER_1:
+                        {
+                            OnMovePlayer1?.Invoke(int.Parse(args.Snapshot.Child("currentPosition").Value.ToString()));
+                            break;
+                        }
+                    case SessionsDataRef.PLAYER_2:
+                        {
+                            OnMovePlayer2?.Invoke(int.Parse(args.Snapshot.Child("currentPosition").Value.ToString()));
+                            break;
+                        }
                 }
             }
         }
         #endregion Listeners for database table: stcrunner_sessions
 
-        private void OnGameStateChanged(GameState p_gameState)
+        private void OnGameStateChanged(GameState pState)
         {
-            if (p_gameState.Equals(GameState.GAME_OVER))
+            //Debug.Log("SessionClientHandler.OnGameOver() ::--> " + pState);
+
+            if (pState == GameState.GAME_OVER)
             {
-                m_isPlaying = false;
-                m_sessionsDB.ChildChanged -= HandleSessionsChildChanged;
-                Debug.Log("SessionClientHandler::--> " + p_gameState);
-                m_sessionsDB.Child(SessionsDataRef.IS_PLAYING).SetValueAsync(false).ContinueWithOnMainThread(task =>
-                {
-
-                });
-
-                OnMovePlayer1?.Invoke(1);
-                return;
+                // Set database to gameState = END
+                m_sessionsDB.Child(SessionsDataRef.GAME_STATE).SetValueAsync("END");
             }
+            else if (pState == GameState.LEADERBOARD)
+            {
+                // Set database to gameState = IDLE
+                m_sessionsDB.Child(SessionsDataRef.GAME_STATE).SetValueAsync("IDLE");
+            }
+            return;
         }
 
         void OnDestroy()
